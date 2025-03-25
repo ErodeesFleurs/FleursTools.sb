30de46a56942788d7e424e9bbe7215cf02fdc90b
ErodeesFleurs <333309908@qq.com>
2025-03-25 16:39:34 +0800

    修改为初始化

--- src/egui/base.rs
+++ src/egui/base.rs
@@ -1,15 +1,8 @@
-pub fn get_windows_id(_: &mlua::Lua,_: ()) -> mlua::Result<u32> {
+pub fn get_windows_id(_: &mlua::Lua,_: ()) -> mlua::Result<i32> {
     let id = unsafe {
-        let raw_window = sdl2::sys::SDL_GL_GetCurrentWindow();
-        sdl2::sys::SDL_GetWindowID(raw_window)
+        let is_ok = sdl2::sys::SDL_InitSubSystem(sdl2::sys::SDL_INIT_VIDEO);
+        is_ok
     };
-
-    if id == 0 {
-        let msg: std::borrow::Cow<'_, str> = unsafe {
-            std::ffi::CStr::from_ptr(sdl2::sys::SDL_GetError()).to_string_lossy()
-        };
-        return Err(mlua::Error::RuntimeError(msg.to_string()));
-    }
 
     Ok(id)
 }
