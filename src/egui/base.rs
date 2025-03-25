dcd19e6fe29be01ba8378e3378e9fee19d9b4dc6
ErodeesFleurs <333309908@qq.com>
2025-03-25 15:40:49 +0800

    添加错误处理

--- src/egui/base.rs
+++ src/egui/base.rs
@@ -4,5 +4,12 @@ pub fn get_windows_id(_: &mlua::Lua,_: ()) -> mlua::Result<u32> {
         sdl2::sys::SDL_GetWindowID(raw_window)
     };
 
+    if id == 0 {
+        let msg: std::borrow::Cow<'_, str> = unsafe {
+            std::ffi::CStr::from_ptr(sdl2::sys::SDL_GetError()).to_string_lossy()
+        };
+        return Err(mlua::Error::RuntimeError(msg.to_string()));
+    }
+
     Ok(id)
 }
