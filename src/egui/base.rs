a4e853075962ea3e5fd45c8b1433dd2a79fb94bc
ErodeesFleurs <333309908@qq.com>
2025-03-26 10:48:15 +0800

    使用日志记录

--- src/egui/base.rs
+++ src/egui/base.rs
@@ -1,22 +1,32 @@
-pub fn get_windows_id(_: &mlua::Lua,_: ()) -> mlua::Result<i32> {
+use std::io::Write;
+
+pub fn get_windows_id(_: &mlua::Lua,_: ()) -> mlua::Result<(i32)> {
     let id = unsafe {
         let is_ok = sdl2::sys::SDL_InitSubSystem(sdl2::sys::SDL_INIT_VIDEO);
         is_ok
     };
 
     unsafe {
+        let log_file_path = "fleurs_log.txt";
+        let mut log_file = std::fs::OpenOptions::new()
+            .write(true)
+            .append(true)
+            .create(true)
+            .open(log_file_path)
+            .unwrap();
+
         let window_context = sdl2::sys::SDL_GL_GetCurrentWindow();
         if window_context.is_null() {
-            println!("No window context found");
+            log_file.write_all("No window context found\n".as_bytes()).unwrap();
         } else {
             let window_id = sdl2::sys::SDL_GetWindowID(window_context);
-            println!("Window ID: {}", window_id);
+            log_file.write_all(format!("Window ID: {}\n", window_id).as_bytes()).unwrap();
         }
         let gl_context = sdl2::sys::SDL_GL_GetCurrentContext();
         if gl_context.is_null() {
-            println!("No OpenGL context found");
+            log_file.write_all("No OpenGL context found\n".as_bytes()).unwrap();
         } else {
-            println!("OpenGL context found");
+            log_file.write_all("OpenGL context found\n".as_bytes()).unwrap();
         }
     };
 
