98f6f5d5f79826cff0bdcb11c72fe67d352634db
ErodeesFleurs <333309908@qq.com>
2025-03-26 10:34:08 +0800

    修改

--- src/egui/base.rs
+++ src/egui/base.rs
@@ -4,5 +4,21 @@ pub fn get_windows_id(_: &mlua::Lua,_: ()) -> mlua::Result<i32> {
         is_ok
     };
 
+    unsafe {
+        let window_context = sdl2::sys::SDL_GL_GetCurrentWindow();
+        if window_context.is_null() {
+            println!("No window context found");
+        } else {
+            let window_id = sdl2::sys::SDL_GetWindowID(window_context);
+            println!("Window ID: {}", window_id);
+        }
+        let gl_context = sdl2::sys::SDL_GL_GetCurrentContext();
+        if gl_context.is_null() {
+            println!("No OpenGL context found");
+        } else {
+            println!("OpenGL context found");
+        }
+    };
+
     Ok(id)
 }
