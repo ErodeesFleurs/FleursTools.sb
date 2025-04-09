a2de87504248565503c52cb579ff20d8492c2ade
ErodeesFleurs <862959461@qq.com>
2025-04-09 23:22:03 +0800

    修改为初始化

--- src/hook/mod.rs
+++ src/hook/mod.rs
@@ -4,15 +4,11 @@ mod state;
 pub fn register_function(lua: &mlua::Lua) -> mlua::Result<mlua::Table> {
     let hook = lua.create_table()?;
 
-    let symbols = lua.create_function(|lua: &mlua::Lua, path: Option<String>|  -> mlua::Result<mlua::Table> {
-        let symbols = symbol::platform_parse_symbols(path);
-        let table = lua.create_table()?;
-        for (sym, addr) in symbols.map_err(|e| mlua::Error::external(e))? {
-            table.set(sym, addr)?;
-        }
-        Ok(table)
+    let init_symbols = lua.create_function(|_: &mlua::Lua, path: Option<String>|  -> mlua::Result<()> {
+        symbol::platform_parse_symbols(path).map_err(|e| mlua::Error::external(e))?;
+        Ok(())
     })?; 
-    hook.set("symbols", symbols)?;
+    hook.set("init_symbols", init_symbols)?;
 
     let test_hook = lua.create_function(|_, (addr, name): (u64, String)| -> mlua::Result<bool> {
         let result = state::hook_func(addr, &name);
